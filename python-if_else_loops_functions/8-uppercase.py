15856c7b23566bcc2267913c8b4ef053dfdb713a
RoPallas <rocio.orquestas@gmail.com>
2023-05-29 11:52:23 -0700

    Python - if/else, loops, functions

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_str = ''
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            upper_str += chr(ord(i) - 32)
+        else:
+            upper_str += i
+    print(upper_str)
