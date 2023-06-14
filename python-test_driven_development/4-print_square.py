0eb3e051d65a0cb766deede3892aff3e305b4ec1
RoPallas <rocio.orquestas@gmail.com>
2023-06-14 12:20:18 -0700

    Task 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -17,9 +17,8 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    if size == 0:
-        print()
     for i in range(size):
         for j in range(size):
-            print("#", end="")
-        print("")
+            print("#", end="" if j < (size - 1) else "\n")
+    if size == 0:
+        print()
