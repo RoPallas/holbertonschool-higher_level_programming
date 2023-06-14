d3c99a09a14eb2d473c92598c8e4bd0a25b80e26
RoPallas <rocio.orquestas@gmail.com>
2023-06-14 11:03:14 -0700

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@
 
 def add_integer(a, b=98):
     """Return the result to addition two int or float values
-    
+
     Float values are cast into a int values
     The b value is optional, by default 98
     Raises:
@@ -15,7 +15,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     a = int(a)
     b = int(b)
 
