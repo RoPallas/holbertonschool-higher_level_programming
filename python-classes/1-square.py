4ff484ed0a92df376e75060bf3df2812fe85f9b6
RoPallas <rocio.orquestas@gmail.com>
2023-06-14 08:11:22 -0700

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """A Square"""
+
+    def __init__(self, size):
+        """Init a Square
+
+        Args:
+            size (int): The size of the square
+        """
+
+        self.__size = size
