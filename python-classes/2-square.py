32aa7c98638b28a1958bbd68cdd7b6f7af16d258
RoPallas <rocio.orquestas@gmail.com>
2023-06-14 08:16:15 -0700

    Task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """A Square"""
+
+    def __init__(self, size=0):
+        """Init a Square
+
+        Args:
+            size (int): The size of the square
+        """
+
+        if isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size > 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
