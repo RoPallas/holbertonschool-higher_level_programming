908095a2028e67094cdf6499593de44dead11f07
RoPallas <rocio.orquestas@gmail.com>
2023-06-24 10:44:53 -0700

    Task 0 and 3

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,5 @@ def read_file(filename=""):
     """function that reads a text file (UTF8)
     and prints it to stdout"""
 
-    with open(filename, encoding="UTF8") as file:
-        text = file.read()
-        print(text)
+    with open(filename, "r", encoding="UTF8") as file:
+        print(file.read(), end="")
