72d71be5b8c80bdcd5577f9a72f8e389c76ea544
RoPallas <rocio.orquestas@gmail.com>
2023-06-29 07:53:02 -0700

    Task 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,7 +24,7 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns the JSON string representation of list_dictionaries"""
+        """Returns the JSON string representation of list_dictionaries"""
 
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
@@ -32,7 +32,7 @@ class Base():
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """writes the JSON string representation of list_objs to a file"""
+        """Writes the JSON string representation of list_objs to a file"""
 
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
@@ -41,3 +41,20 @@ class Base():
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+
+        if json_string in None or json_string == []:
+            return "[]"
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+
+        if dictionary:
+            new_obj = cls(1, 1) if cls.__name__ == "Rectangle" else cls(1)
+            new_obj.update(**dictionary)
+            return new_obj
