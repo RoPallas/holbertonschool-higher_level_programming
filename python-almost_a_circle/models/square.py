9ecbc55aa169f531e511cf17572b38c57442bc15
RoPallas <rocio.orquestas@gmail.com>
2023-06-29 06:34:57 -0700

    Task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,10 +11,23 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get the size of the square"""
+
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size"""
+
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return the rectangle description"""
 
         name = self.__class__.__name__
         xy = f"{self.__x}/{self.__y}"
-        size = f"{self.__width}"
+        size = f"{self.width}"
         return f"[{name}] ({self.id}) {xy} - {size}"
