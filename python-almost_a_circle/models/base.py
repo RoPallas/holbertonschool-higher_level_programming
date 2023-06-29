7e59c9eb13d757b2d1c001a58f61d1b1d5c40e80
RoPallas <rocio.orquestas@gmail.com>
2023-06-29 07:55:23 -0700

    Task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -46,7 +46,7 @@ class Base():
     def from_json_string(json_string):
         """Returns the list of the JSON string representation json_string"""
 
-        if json_string in None or json_string == []:
+        if json_string is None or json_string == []:
             return "[]"
         return json.loads(json_string)
 
