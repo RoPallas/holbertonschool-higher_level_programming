4d5f976d8f32766b81c86ab69d0c5ee136c56926
RoPallas <rocio.orquestas@gmail.com>
2023-06-29 07:24:05 -0700

    Tasks 15 and 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Class Base"""
+import json
 
 
 class Base():
@@ -20,3 +21,23 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """"""
+
+        if list_objs is None:
+            data = "[]"
+        else:
+            data = [obj.to_dictionaty() for obj in list_objs]
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            jsonfile.write(Base.to_json_string(data))
