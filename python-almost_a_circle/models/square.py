3a2c64044c07e5e0d85c0663dbe6867542054dbe
RoPallas <rocio.orquestas@gmail.com>
2023-06-29 06:46:12 -0700

    Task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,6 +28,33 @@ class Square(Rectangle):
         """Return the rectangle description"""
 
         name = self.__class__.__name__
-        xy = f"{self.__x}/{self.__y}"
+        xy = f"{self.x}/{self.y}"
         size = f"{self.width}"
         return f"[{name}] ({self.id}) {xy} - {size}"
+
+    def update(self, *args, **kwargs):
+        """Update the Rectangle.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3th argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+
+        if args:
+            arg_names = ["id", "size", "x", "y"]
+            for i, arg in enumerate(args):
+                if arg is None:
+                    self.__init__(self.size, self.x, self.y)
+                else:
+                    setattr(self, arg_names[i], arg)
+
+        elif kwargs:
+            for k, v in kwargs.items():
+                if k == "id" and v is None:
+                    self.__init__(self.size, self.x, self.y)
+                else:
+                    setattr(self, k, v)
